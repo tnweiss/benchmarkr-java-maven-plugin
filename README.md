c1df17e6e9057ffc594e67cf3ae72192b27a555b
Tyler Weiss <tnweiss5@gmail.com>
2022-05-22 22:28:11 -0700

    Version 0.0.1

--- README.md
+++ README.md
@@ -1,17 +1,38 @@
-```xml
-      <plugin>
-        <groupId>benchmarkr</groupId>
-        <artifactId>benchmarkr-maven-plugin</artifactId>
-        <version>0.0.1-SNAPSHOT</version>
+![](images/BenchmarkrMavenPlugin.svg)
 
-        <executions>
-          <execution>
-            <id>benchmarkr</id>
-            <phase>test</phase>
-            <goals>
-              <goal>benchmark</goal>
-            </goals>
-          </execution>
-        </executions>
-      </plugin>
-```
+## Overview
+
+Set standards for your software.
+
+This repository provides a benchmarking framework for Java. See [Benchmarkr CPP](https://github.com/tnweiss/Benchmarkr-cpp)
+to get the binaries used to set up ELK indices and upload test results.
+
+The goal of this project is to track **relative performance**, not absolute performance. No two runs, even on the same machine,
+will yield the same results. This project attempts to highlight significant trends and major anomalies by allowing
+developers to constantly benchmark their software.
+
+The screenshot below shows the default dashboard that comes with this project. It provides basic information about
+which tests have outperformed expectations and which have underperformed.
+
+![](https://github.com/tnweiss/Benchmarkr-cpp/raw/master/images/BenchmarkrDashboard.PNG)
+
+The line graph, in the screenshot above, becomes more useful when you filter on a single test. In the screenshot below
+we filtered by `TestName : NormalizeDataTest` and this is what our line graph looked like...
+
+![](https://github.com/tnweiss/Benchmarkr-cpp/raw/master/images/BenchmarkrDashboardLineGraph.PNG)
+
+And then our table, gave us some insight into individual results that could then be further filtered and organized.
+
+![](https://github.com/tnweiss/Benchmarkr-cpp/raw/master/images/BenchmarkrDashboardTable.PNG)
+
+## Related Benchmarkr Projects
+
+- [Benchmarkr Jetbrains Plugin](https://github.com/tnweiss/benchmarkr-jetbrains-plugin) -
+  Jetbrains plugin for Intellij and CLION that makes it easier to run individual tests from the gutter
+- [Benchmarkr Configuration](https://github.com/tnweiss/benchmarkr-configuration) -
+  Centralized configuration for elastic dashboards and indices
+
+## Usage
+
+See [Benchmarkr Java - Installing Benchmarkr](https://github.com/tnweiss/benchmarkr-java#installing-benchmarkr) 
+for instructions on how to incorporate this plugin into your project.
--- src/main/java/com/benchmarkr/BenchmarkrMojo.java
+++ src/main/java/com/benchmarkr/BenchmarkrMojo.java
@@ -1,8 +1,16 @@
 package com.benchmarkr;
 
+import java.io.File;
+import java.net.URL;
+import java.net.URLClassLoader;
+
 import com.github.benchmarkr.BenchmarkrCore;
+import com.github.benchmarkr.console.Consoles;
+import com.github.benchmarkr.test.plan.TestPlanParameters;
 
 import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
@@ -14,11 +22,67 @@ public class BenchmarkrMojo extends AbstractMojo {
   @Parameter(defaultValue = "${project}", required = true, readonly = true)
   MavenProject project;
 
-  public void execute() {
-    getLog().info("Running Benchmarking Tests");
-    getLog().info("Scanning for tests in package " + project.getGroupId());
+  @Parameter(defaultValue = BenchmarkrCore.PACKAGE_PROPERTY_DEFAULT, property = BenchmarkrCore.PACKAGE_PROPERTY)
+  String packageName;
 
-    String[] args = {"-p", project.getGroupId()};
-    BenchmarkrCore.main(args);
+  @Parameter(property = BenchmarkrCore.CLASS_PROPERTY, defaultValue = BenchmarkrCore.CLASS_PROPERTY_DEFAULT)
+  String className;
+
+  @Parameter(defaultValue = BenchmarkrCore.METHOD_PROPERTY_DEFAULT, property = BenchmarkrCore.METHOD_PROPERTY)
+  String methodName;
+
+  @Parameter(defaultValue = BenchmarkrCore.ITERATIONS_PROPERTY_DEFAULT, property = BenchmarkrCore.ITERATIONS_PROPERTY)
+  int iterations;
+
+  @Parameter(defaultValue = BenchmarkrCore.CONSOLE_PROPERTY_DEFAULT, property = BenchmarkrCore.CONSOLE_PROPERTY)
+  String console;
+
+  @Parameter(defaultValue = BenchmarkrCore.RECORD_PROPERTY_DEFAULT, property = BenchmarkrCore.RECORD_PROPERTY)
+  boolean record;
+
+  @Parameter(defaultValue = BenchmarkrCore.IGNORE_FAILURES_PROPERTY_DEFAULT,
+      property = BenchmarkrCore.IGNORE_FAILURES_PROPERTY)
+  boolean ignoreFailures;
+
+  public void execute() throws MojoExecutionException, MojoFailureException {
+    getLog().info("Running Benchmarking Tests");
+
+    TestPlanParameters testPlanParameters = TestPlanParameters.builder()
+        .console(Consoles.system())
+        .classLoader(getClassLoader())
+        .packageName(packageName == null || packageName.isBlank() ? project.getGroupId() : packageName)
+        .className(className)
+        .methodName(methodName)
+        .iterations(iterations)
+        .console(Consoles.from(console))
+        .recordResults(record)
+        .ignoreFailure(ignoreFailures)
+        .build();
+
+    getLog().info("Scanning for tests in package " + testPlanParameters.getPackageName());
+
+    if (BenchmarkrCore.main(testPlanParameters) != 0) {
+      throw new MojoFailureException("Benchmark tests failed");
+    }
+  }
+
+  /**
+   * Solution pulled from <a href="https://stackoverflow.com/questions/871708/maven-plugin-cant-load-class">Stack Overflow</a>
+   *   and modified. Get a classloader for the dependent projects test sources.
+   *
+   * @throws MojoExecutionException on any errors loading the test classpath
+   * @return the dependent projects test resource classpath
+   */
+  private ClassLoader getClassLoader() throws MojoExecutionException
+  {
+    try {
+      String testOutputDirectory = project.getBuild().getTestOutputDirectory();
+      getLog().debug("Loading test resources from " + testOutputDirectory);
+
+      URL[] testOutputUrls = {new File(testOutputDirectory).toURI().toURL()};
+      return new URLClassLoader(testOutputUrls, getClass().getClassLoader());
+    } catch (Exception e) {
+      throw new MojoExecutionException("Couldn't create a classloader.", e);
+    }
   }
 }
